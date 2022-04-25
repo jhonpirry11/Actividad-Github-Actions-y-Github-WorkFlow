e113d550617c0343c129439d2ce6cbe50a331ce9
jhonpirry11 <jaruizb01@libertadores.edu.co>
2022-04-24 19:43:45 -0500

    Update Hola.java
    terminamos la tarea

--- Hola.java
+++ Hola.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Hola {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-System.out.println ("Hola mundo");
+System.out.println ("Hola mundo, bienvenido a los libertadores");
         
         }
     }
